dffd1f6a5a2522982fd5afafa2837ccdded68ebc
猛犸象 <1664361475@qq.com>
2021-09-07 13:14:30 +0800

    jiaqiangban

--- send_report.py
+++ send_report.py
@@ -16,19 +16,18 @@ class DingRobot:
         return case_error
 
     def send_report(self):
-        headers = {"Content-Type": "application/json;charset=utf-8"}
-        content = {
-
-            "msgtype": "link",
-            "link": {
-                "text": "账号jenkisn5,密码123456",
-                "title": "猛犸象" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                "picUrl": "",
-                "messageUrl": "http://jenkisn5:123456@8.129.214.240:9000/job/wework/allure/"
-            }
-        }
         error = self.get_allure()
-        if error > 0:
+        if error >0:
+            headers = {"Content-Type": "application/json;charset=utf-8"}
+            content = {
+                "msgtype": "link",
+                "link": {
+                    "text": "账号jenkisn5,密码123456",
+                    "title": "猛犸象" + datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                    "picUrl": "",
+                    "messageUrl": "http://jenkisn5:123456@8.129.214.240:9000/job/wework/allure/"
+                }
+            }
             response = requests.post(self.ding, headers=headers, data=json.dumps(content))
         else:
             print('无报错')
