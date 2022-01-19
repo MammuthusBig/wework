7d78b69c1c3927293574f6f04110b987e0ed4989
猛犸象 <1664361475@qq.com>
2022-01-19 15:41:16 +0800

    sss

--- send_report.py
+++ send_report.py
@@ -6,7 +6,7 @@ import requests
 
 class DingRobot:
     def __init__(self):
-        self.allure = "http://jenkisn5:123456@8.129.214.240:9000/job/wework/allure/widgets/suites.json"
+        self.allure = "http://jenkisn5:123456@8.129.214.240:9000/job/wework/allure/widgets/suites.json"     
         self.ding = 'https://oapi.dingtalk.com/robot/send?access_token=' \
                     'a5eb6e38be242dcf3a0ceaa1035a8c3093430de8da7384bf0b710711d4885c49'
         self.error = self.get_allure_error()
@@ -28,7 +28,7 @@ class DingRobot:
                     "messageUrl": "http://jenkisn5:123456@8.129.214.240:9000/job/wework/allure/"
                 }
             }
-            response = requests.post(self.ding, headers=headers, data=json.dumps(content))
+            requests.post(self.ding, headers=headers, data=json.dumps(content))
         else:
             print('无报错')
 
